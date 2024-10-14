33013d6dfcb54a2b05d8b566d3c46f9114c1f842
minhtran-213 <minhtran21301@gmail.com>
2024-10-14 10:36:12 +0700

    feat: Add CORS setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,34 @@
 import express, { Request, Response } from 'express';
 
 import { Client } from '@notionhq/client';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+
+const whitelist = ['http://localhost:3000', process.env.PRODUCTION_REACT_APP_URL];
+
+// CORS options
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
